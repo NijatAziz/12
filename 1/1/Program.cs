a556aa1f41701f527a0b9f7fa192331f8d70f0b3
NijatAziz <nijatfaz@code.edu.az>
2023-07-12 23:09:52 +0400

    homework

---
+++ 1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿{
+    public static int Main(string[] args)
+    {
+        Console.Write("enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("the entered number is even.");
+        }
+        else
+        {
+            Console.WriteLine("the entered number is odd.");
+        }
+
+        return 0;
+    }
+}
